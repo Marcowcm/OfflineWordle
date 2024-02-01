530ad4e7eefabd4055664736318f114d16f389b4
Marcowcm <marcowcm@gmail.com>
2024-02-02 01:44:00 +0800

    bugfix

--- woordle/src/components/keyboard/Key.tsx
+++ woordle/src/components/keyboard/Key.tsx
@@ -8,7 +8,7 @@ export const Key = ({ value, onClick, status }: Props) => {
   const clickHandler = () => {
     onClick(value);
   };
-  const className = "key col center-align" + status;
+  const className = "key col center-align " + status;
   return (
     <button key={value} onClick={clickHandler} className={className} value = {value}>
       {value==="ENTER"? <i className="material-icons">keyboard_return</i> :value==="<--"? <i className="material-icons">undo</i> : value}
